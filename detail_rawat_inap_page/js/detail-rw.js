1b0ee483d4e9b494de302cd547a4d0c1855ede52
Febrianto100 <febrianto18@mhs.ubharajaya.ac.id>
2021-12-02 17:53:01 +0700

    fixing starter-template and bug in navbar of hospitalization details page view

--- detail_rawat_inap_page/js/detail-rw.js
+++ detail_rawat_inap_page/js/detail-rw.js
@@ -1,8 +1,8 @@
 // rotate up-arrow-icon on event collapse accordion
-$('.collapse').on('show.bs.collapse', function () {
+$('.multi-collapse').on('show.bs.collapse', function () {
   this.previousElementSibling.querySelector('.up-arrow-icon').style.transform = "rotate(0deg)";
 })
 
-$('.collapse').on('hide.bs.collapse', function () {
+$('.multi-collapse').on('hide.bs.collapse', function () {
   this.previousElementSibling.querySelector('.up-arrow-icon').style.transform = "rotate(180deg)";
 })
