913dda493e0ee2964ff5b735b259e23b7d1e0af6
Febrianto100 <febrianto18@mhs.ubharajaya.ac.id>
2021-12-04 21:38:02 +0700

    manage relative url images and move content detail's detail-rw.js file to main.js

--- template_view-febri/js/main.js
+++ template_view-febri/js/main.js
@@ -6,4 +6,13 @@ ButtonsTypeOfHospitalization.forEach((btnType) => {
       event.target.firstElementChild.checked = true;
     }
   })
-})
+});
+
+// rotate up-arrow-icon on event collapse accordion
+$('.multi-collapse').on('show.bs.collapse', function () {
+  this.previousElementSibling.querySelector('.up-arrow-icon').style.transform = "rotate(0deg)";
+});
+
+$('.multi-collapse').on('hide.bs.collapse', function () {
+  this.previousElementSibling.querySelector('.up-arrow-icon').style.transform = "rotate(180deg)";
+});
